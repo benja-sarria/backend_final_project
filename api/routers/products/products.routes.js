65397ffd0d017f0398c78be0d57d201293ec01c3
Benja Sarría Ferrer <benja.sarria@live.com.ar>
2022-03-06 03:41:31 -0300

    (Fix) - Correcciones finales en ruta de productos post a los fines de obtener respuesta en el cliente

--- api/routers/products/products.routes.js
+++ api/routers/products/products.routes.js
@@ -64,6 +64,7 @@ router.post("/", async (req, res) => {
                         price: Number(req.body.price),
                     };
                     const response = await req.products.saveProduct(newProduct);
+                    res.json(response);
                 }
             }
         } else {
