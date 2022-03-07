d5a023e2db85e28c448c503b2a51856db5159353
Benja Sarría Ferrer <benja.sarria@live.com.ar>
2022-03-07 12:19:58 -0300

    (Fix) - Modificaciones en allowaccess

--- api/middlewares/allowAccess.js
+++ api/middlewares/allowAccess.js
@@ -1,23 +1,13 @@
 export const allowAccess = (req, res, next) => {
-    // Website you wish to allow to connect
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-
-    // Request methods you wish to allow
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-
-    // Request headers you wish to allow
-    res.setHeader(
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
         "Access-Control-Allow-Headers",
-        "X-Requested-With,content-type"
+        "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
     );
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader("Access-Control-Allow-Credentials", true);
-
-    // Pass to next layer of middleware
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, DELETE"
+    );
+    res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
     next();
 };
